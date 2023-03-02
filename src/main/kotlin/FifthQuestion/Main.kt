30b0d7ef91bad5ed0cfbce4ecbc39ec81ef66896
Ygor Doring <developer@ygordoring.dev>
2023-03-02 17:18:01 -0300

    Fifth question resolution

---
+++ src/main/kotlin/FifthQuestion/Main.kt
@@ -0,0 +1,23 @@
+package FifthQuestion
+
+fun main() {
+    val words = "TargetSistemas"
+    var invertion = ""
+
+    for (i in words.length - 1 downTo 0) {
+        invertion += words[i]
+    }
+
+    println(invertion)
+}
+
+//A primeiro momento pensei em fazer utilizando a função reversed(), mas depois percebi a recomendação
+// e fiz da outra sem... De qualquer forma, segue abaixo como ficaria
+
+//  fun main() {
+//      val word = "exemplo"
+//      val invertion = word.reversed()
+//
+//      println(invertion)
+//  }
+
