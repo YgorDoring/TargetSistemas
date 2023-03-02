9789720ee18b239617e37015ba98ecbea09e3ac4
Ygor Doring <developer@ygordoring.dev>
2023-03-02 17:23:39 -0300

    Fifth question resolution

--- src/main/kotlin/FifthQuestion/Main.kt
+++ src/main/kotlin/FifthQuestion/Main.kt
@@ -12,7 +12,7 @@ fun main() {
 }
 
 //A primeiro momento pensei em fazer utilizando a função reversed(), mas depois percebi a recomendação
-// e fiz da outra sem... De qualquer forma, segue abaixo como ficaria
+//e fiz da outra forma sem a função... De qualquer forma, segue abaixo como ficaria:
 
 //  fun main() {
 //      val word = "exemplo"
